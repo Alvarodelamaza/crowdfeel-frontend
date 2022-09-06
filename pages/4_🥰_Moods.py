1a0aadfdce1ae420f844a8554e8bb093b243fb5a
Alvaro de la Maza <alvarodlmaza@gmail.com>
2022-09-06 17:52:04 +0200

    chart added

--- pages/4_🥰_Moods.py
+++ pages/4_🥰_Moods.py
@@ -1,15 +1,15 @@
 
 from pickletools import read_bytes1
 import streamlit as st
-import datetime
+import seaborn as sns
 #import time
 import requests
 import pandas as pd
 import numpy as np
-import pydeck as pdk
+
 import matplotlib.pyplot as plt
 import numpy as np
-import altair as alt
+
 #import streamlit.components.v1 as components
 
 import plotly.express as px
@@ -26,26 +26,47 @@ st.set_page_config(
          'About': "## Population sentiment analysis using tweets \n Bootcamp project developed by: \n Alvaro de la Maza, Angelo Darriet, Beauregard Sangkala and Tjebbe Lodeizen"
      }
  )
+#Header
+st.image('palette_header.png')
+
+#Blank space
+c=st.empty()
+c.write(' ')
+c=st.empty()
+c.write(' ')
+c=st.empty()
+c.write(' ')
+c=st.empty()
+c.write(' ')
+c=st.empty()
+c.write(' ')
 
 # Title and subtitle
-title='👥 Crowdfeel 👥'
-subtitle="The tool to track people's sentiment through Twitter 💬"
-st.markdown(f"<h1 style='text-align: center;font-size: 60px;'>{title}</h1>", unsafe_allow_html=True)
-st.markdown(f"<h1 style='text-align: center;font-size: 35px;'>{subtitle}</h1>", unsafe_allow_html=True)
+title='Extract sentiments from hashtag and location'
+subtitle="Our tool is able to extract whether the sentiment behind a tweet is: 😃 Happiness, 🤬 Hate, 😍 Love, 😐 Neutrality, 😭 Sadness, 😲 Surprise or 😱 Worry "
+st.markdown(f"<h1 style='text-align: center;font-size: 60px;color: #0B0500';'>{title}</h1>", unsafe_allow_html=True)
+st.markdown(f"<h1 style='text-align: center;font-size: 35px;color: #0B0500';'>{subtitle}</h1>", unsafe_allow_html=True)
 
+#Blank space
+c=st.empty()
+c.write(' ')
+c=st.empty()
+c.write(' ')
+c=st.empty()
+c.write(' ')
 
 # Search form hashtag emotions
 with st.form("search_form_emotions"):
 
     # Date filter
-    st.markdown(f"<h1 style='text-align: center;font-size: 30px;'>When? 📆</h1>", unsafe_allow_html=True)
-    col1, col2 = st.columns(2)
-    date_start = col1.date_input(' From...', value=datetime.datetime(2022, 8, 1, 12, 10, 20))
-    date_finish = col2.date_input(' ...to', value=datetime.datetime(2022, 8, 31, 12, 10, 20))
+    #st.markdown(f"<h1 style='text-align: center;font-size: 30px;'>When? 📆</h1>", unsafe_allow_html=True)
+    #col1, col2 = st.columns(2)
+    #date_start = col1.date_input(' From...', value=datetime.datetime(2022, 8, 1, 12, 10, 20))
+    #date_finish = col2.date_input(' ...to', value=datetime.datetime(2022, 8, 31, 12, 10, 20))
 
     # Hashtag filter
     st.markdown(f"<h1 style='text-align: center;font-size: 30px;'>Hashtag? #️⃣</h1>", unsafe_allow_html=True)
-    col3, col4 = st.columns(2)
+    col1 , col3, col4 = st.columns(3)
     hashtag=col3.text_input(''' Hashtag you want to search ''')
 
     col11, col21 , col23,col34, col31 = st.columns(5)
@@ -65,32 +86,32 @@ with st.form("search_form_emotions"):
                 res=requests.get(url).json()
                 print(res)
                 tweet=res['tweet']
-                emotion=res['label']
+                emotions=res['label']
             st.success('Emotions extracted succesfully!',icon='✅')
 
             emojy = []
             word = []
-            for twee in tweet:
+            for emotion in emotions:
             # Set the emojis
-                if emotion=='0':
+                if emotion==0:
                     emojy.append('😃')
-                    word.append('Happy')
-                elif emotion== '1':
+                    word.append('Happiness')
+                elif emotion== 1:
                     emojy.append('🤬')
                     word.append('Hate')
-                elif emotion=='2':
+                elif emotion==2:
                     emojy.append('😍')
                     word.append('Love')
-                elif emotion=='3':
+                elif emotion==3:
                     emojy.append('😐')
                     word.append('Neutral')
-                elif emotion=='4':
+                elif emotion==4:
                     emojy.append('😭')
-                    word.append('Sad')
-                elif emotion=='5':
+                    word.append('Sadness')
+                elif emotion==5:
                     emojy.append('😲')
                     word.append('Surprise')
-                else:
+                elif emotion==6:
                     emojy.append('😱')
                     word.append('Worry')
 
@@ -100,10 +121,26 @@ with st.form("search_form_emotions"):
             with col1:
                 with st.expander(" See random Tweets"):
                     for twee, emotion, labels in zip(tweet,emojy, word):
-                        text=f'''{twee} is {labels}'''.replace("\n","")
+                        text=f'''{twee} implies {labels} {emotion}'''.replace("\n","")
                         text_html = f'<p style="font-family:sans-serif; font-size: 20px; border-radius: 25px; border: 2px solid; padding: 20px;">{text}</p>'
                         st.markdown(text_html, unsafe_allow_html=True)
 
             #Column #2 with charts
             with col2:
-                st.bar_chart(data=emotion)
+                sentence_dictionary = {}
+                word_counts = 0
+                for item in word:
+                    if item in sentence_dictionary:
+                        sentence_dictionary[item][0] += 1
+                    else:
+                        sentence_dictionary[item] = [1]
+                print(len(word))
+                print(word)
+                print(sentence_dictionary)
+                word_df=pd.DataFrame(sentence_dictionary)
+                # Bar chart
+                sns.set(font_scale=2)
+                colors={'Happiness':'#AAF683','Hate':'#F74052' ,'Love':'#FF7738','Neutral':'#FFD952','Sadness':'#51CBDB','Surprise':'#8312ED','Worry':'#9FFFCB'}
+                fig = plt.figure(figsize=(10, 4))
+                sns.barplot(x=word_df.columns,y=word_df.values[0],palette=colors)
+                st.pyplot(fig)
